2ff516c28159aa0c9c36e92f5b4f08e12521db76
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-03-09 10:21:31 +0700

    Transaction Index

---
+++ resources/views/pages/dashboard/transaction/index.blade.php
@@ -0,0 +1,100 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Transaction') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="script">
+        <script>
+            // Ajax Data Tables
+            var datatable = $('#crudTable').DataTable({
+                ajax: {
+                    url: '{!! url()->current() !!}'
+                },
+                columns: [{
+                        data: 'id',
+                        name: 'id',
+                        width: '5%'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'email',
+                        name: 'email'
+                    },
+                    {
+                        data: 'address',
+                        name: 'address'
+                    },
+                    {
+                        data: 'phone',
+                        name: 'phone'
+                    },
+                    {
+                        data: 'courier',
+                        name: 'courier'
+                    },
+                    {
+                        data: 'total_price',
+                        name: 'total_price'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false,
+                        width: '25%'
+                    }
+                ]
+            })
+        </script>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (session()->has('success'))
+                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+                    role="alert">
+                    <div class="flex">
+                        <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path
+                                    d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                            </svg></div>
+                        <div>
+                            <p class="font-bold">Our privacy policy has changed</p>
+                            <p class="text-sm">Make sure you know how these changes affect you.</p>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            <div class="shadow overflow-hidden sm-rounded-md">
+                <div class="px-4 py-5 bg-white sm:p-6">
+                    <table id="crudTable">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Alamat</th>
+                                <th>No Handphone</th>
+                                <th>Kurir</th>
+                                <th>Total Harga</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProductGalleryController;
 
 /*
@@ -33,6 +34,9 @@ Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('das
         Route::resource('product', ProductController::class);
         Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
             'index', 'create', 'store', 'destroy'
+        ]);
+        Route::resource('transaction', TransactionController::class)->shallow()->only([
+            'index', 'show', 'edit', 'update'
         ]);
     });
 });
